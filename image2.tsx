48c3c88ecfd94de58eaa3f5ba94fa9dfdd491697
kuriboo <kuriboo5@gmail.com>
2024-10-04 22:31:19 +0900

    Add or update image2.tsx

--- image2.tsx
+++ image2.tsx
@@ -1,26 +1,44 @@
 ```tsx
 import React from 'react';
 
-// TypeScript interface for component props
+// 定義する型
 interface LogoProps {
+  altText: string;
   width?: string;
   height?: string;
-  altText?: string;
+  className?: string;
 }
 
-const Logo: React.FC<LogoProps> = ({ width = '200px', height = '50px', altText = '企業ロゴ' }) => {
+// 再利用可能なロゴコンポーネント
+const Logo: React.FC<LogoProps> = ({ altText, width = '200px', height = '50px', className = '' }) => {
   return (
-    <div className={`flex items-center justify-center`}>
+    <div className={`flex items-center ${className}`}>
       <img
-        src="/path/to/logo/image.jpg" // 画像パスを変更してください
+        src="/path/to/logo.png" // ロゴ画像のパス
         alt={altText}
         width={width}
         height={height}
-        className={`object-contain`} // Tailwind CSSでスタイリング
+        className="object-contain"
       />
+      <span className="ml-2 text-black">株式会社ぎょうせい</span>
     </div>
   );
 };
 
 export default Logo;
+```
+
+```tsx
+// 使用例
+import Logo from './Logo';
+
+const App: React.FC = () => {
+  return (
+    <div className="p-4">
+      <Logo altText="企業ロゴ" className="custom-class" />
+    </div>
+  );
+};
+
+export default App;
 ```
