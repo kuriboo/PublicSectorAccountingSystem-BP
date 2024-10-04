0195e4e0e452b10470d7110e13a951c8c250f058
kuriboo <kuriboo5@gmail.com>
2024-10-04 19:07:18 +0900

    Add or update 公営企業会計システムVer5_18_給排水-image037.tsx

---
+++ 公営企業会計システムVer5_18_給排水-image037.tsx
@@ -0,0 +1,27 @@
+```tsx
+import React from 'react';
+
+// プロパティの型定義
+interface LogoProps {
+  src: string;
+  alt: string;
+  width?: number;
+  height?: number;
+  className?: string;
+}
+
+// Logoコンポーネント定義
+const Logo: React.FC<LogoProps> = ({ src, alt, width = 100, height = 50, className = '' }) => {
+  return (
+    <img
+      src={src}
+      alt={alt}
+      width={width}
+      height={height}
+      className={`object-contain ${className}`} // Tailwind CSSを使用してスタイリング
+    />
+  );
+};
+
+export default Logo;
+```
