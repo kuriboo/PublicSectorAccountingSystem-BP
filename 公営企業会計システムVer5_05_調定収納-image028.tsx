8e62e957b3ab1ebadd5150eee2126f71cbfd04a3
kuriboo <kuriboo5@gmail.com>
2024-10-04 23:59:03 +0900

    Add or update 公営企業会計システムVer5_05_調定収納-image028.tsx

---
+++ 公営企業会計システムVer5_05_調定収納-image028.tsx
@@ -0,0 +1,30 @@
+```tsx
+import React from 'react';
+
+// 型定義
+type DividerProps = {
+  color?: string;
+  height?: string;
+  margin?: string;
+};
+
+// コンポーネント定義
+const Divider: React.FC<DividerProps> = ({
+  color = 'bg-blue-300',
+  height = '2px',
+  margin = '4px 0',
+}) => {
+  return (
+    <div
+      className={`w-full ${color}`}
+      style={{
+        height,
+        margin,
+        borderBottom: '4px solid black',
+      }}
+    />
+  );
+};
+
+export default Divider;
+```
