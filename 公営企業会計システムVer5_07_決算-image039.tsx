5c7b33102b65ba50c518d14f2250a3fa01e16e41
kuriboo <kuriboo5@gmail.com>
2024-10-04 21:43:40 +0900

    Add or update 公営企業会計システムVer5_07_決算-image039.tsx

---
+++ 公営企業会計システムVer5_07_決算-image039.tsx
@@ -0,0 +1,28 @@
+```tsx
+import React from 'react';
+
+interface LogoProps {
+  // ロゴのURLを指定するプロパティ
+  logoUrl: string;
+  // ロゴの代替テキストを指定するプロパティ
+  altText: string;
+  // 追加のクラス名を指定するプロパティ（例: Tailwind CSSクラス）
+  className?: string;
+}
+
+const GyouseiLogo: React.FC<LogoProps> = ({ logoUrl, altText, className }) => {
+  return (
+    <div className={`flex items-center ${className}`}>
+      {/* ロゴ画像 */}
+      <img src={logoUrl} alt={altText} className="h-16 w-auto mr-4" />
+      {/* テキスト部分 */}
+      <div>
+        <div className="text-xl font-bold">株式会社ぎょうせい</div>
+        <div className="text-sm">システム事業部</div>
+      </div>
+    </div>
+  );
+};
+
+export default GyouseiLogo;
+```
